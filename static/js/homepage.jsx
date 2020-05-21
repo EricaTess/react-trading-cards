c10fdff56ecd5a44e57b3386268917c01789d3b8
Erica Sanchez <sanchez.ericat@gmail.com>
2020-05-21 16:01:24 -0700

    updated homepage html to change text, added image and link

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -4,7 +4,9 @@ class Homepage extends React.Component {
   render() {
     return (
       <div>
-        This component needs to be implemented.
+        <p className="greeting">Welcome to the homepage.</p>
+        <a id="homepage-link" href="/cards">Check out the cards</a>
+        <img id="ballonicorn-img" src="/static/img/balloonicorn.jpg"/>
       </div>
     );
   }
